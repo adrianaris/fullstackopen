84c01887d4b4a831addac48998fe062830fe11d3
Adrian Serbanescu <adrian@adrianserbanescu.com>
2021-05-30 10:31:53 +0200

    Add Phonebook 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,28 +1,45 @@
 import React, { useState } from 'react'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import Filter from './components/Filter'
 
-const PersonForm = () => {
-  
-}
 
-const Person = ({ person }) => {
-  return (
-    <li>{person.name} {person.number}</li>
-  )
-}
 
-const Persons = ({ persons }) => {
-  return (
-      <ul>
-	{persons.map(person =>
-	<Person key={person.name.split('').reduce((x,y)=>x+y.charCodeAt(0),0)} person={person} /> 
-	)}
-      </ul>
-  )
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
@@ -53,30 +70,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div> 
-	filter shown with <input
-	  value={filter}
-	  onChange={handleFilter} 
-	/>
-      </div>
+      <Filter filter={filter} handleFilter={handleFilter} /> 
       <h3>Add a new</h3>
-      <form onSubmit={addPerson}>
-	<div>
-	  name: <input
-	    value={newName}
-	    onChange={handleNewName}
-	  />
-	</div>
-	<div>
-	  number: <input
-	    value={newNumber}
-	    onChange={handleNewNumber}
-	  />
-	</div>
-	<div>
-	  <button type="submit">add</button>
-	</div>
-      </form>
+      <PersonForm 
+      addPerson={addPerson} newName={newName} handleNewName={handleNewName} newNumber={newNumber} handleNewNumber={handleNewNumber}
+      /> 
       <h3>Numbers</h3>
       <Persons persons={peopleToShow} />
     </div>
---
+++ part2/phonebook/src/components/Filter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Filter = ({ filter, handleFilter }) => {
+  return (
+    <>
+      filter shown with <input
+	value={filter}
+	onChange={handleFilter}
+      />
+    </>
+  )
+}
+
+export default Filter
