00bd6c3d99675ed07700608a285e6893f3c5829f
Adrian Serbanescu <adrian@adrianserbanescu.com>
2021-06-01 19:51:33 +0200

    Spelling

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -3,31 +3,31 @@ import axios from 'axios'
 
 // JUST WRITE A DIFFERENT COMPONENT FOR THE 2.13 BUTTONS
 
-const Countrie = ({ countrie }) => {
+const Country = ({ country, setFilter }) => {
   return (
     <li>
-      {countrie.name}
+      {country.name} <Button country={country} setFilter={setFilter} />
     </li>
   )
 }
 
-const FilteredCountrie = ({ countrie }) => {
+const FilteredCountry = ({ country }) => {
   return (
     <>
-      <h1>{countrie.name}</h1>
-      <div>capital {countrie.capital}</div>
-      <div>population {countrie.population}</div>
+      <h1>{country.name}</h1>
+      <div>capital {country.capital}</div>
+      <div>population {country.population}</div>
       <h2>Languages</h2>
       <ul>
-        {countrie.languages.map(language => 
+        {country.languages.map(language => 
 	<li key={language.name.split('').reduce((x,y)=>x+y.charCodeAt(0), 0)}>
 	    {language.name}
 	  </li>
 	)}
       </ul>
       <img 
-	src={countrie.flag} 
-	alt={countrie.name} 
+	src={country.flag} 
+	alt={country.name} 
 	width='100'
 	heigth='100'
       />
@@ -35,40 +35,59 @@ const FilteredCountrie = ({ countrie }) => {
   )
 }
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, setFilter }) => {
   return (
     countries.length === 1
-      ? countries.map(countrie =>
-	<FilteredCountrie
-	  key={countrie.name.split('').reduce((x,y)=>x+y.charCodeAt(0),0)}
-	  countrie={countrie} 
+      ? countries.map(country =>
+	<FilteredCountry
+	  key={country.name.split('').reduce((x,y)=>x+y.charCodeAt(0),0)}
+	  country={country} 
 	/>	
 	)
       : countries.length > 10
 	  ? <div>Too many mathces, specify another filter</div>
 	  : <ul>
-	      {countries.map(countrie =>
-	      <Countrie 
-		key={countries.indexOf(countrie)}
-		countrie={countrie} 
+	      {countries.map(country =>
+	      <Country 
+		key={countries.indexOf(country)}
+		country={country} 
+		setFilter={setFilter}
 		  />
 	      )}
 	    </ul>
   )
 }
 
+const Button = ({ country, setFilter }) => {
+  return (
+    <button onClick={() => setFilter(country.name)} > 
+      show
+    </button>
+  )
+}
+
 const App = () => {
   const [countries, setCountries] = useState([])
   const [filter, setFilter] = useState('')
+  const [weather, setWeather] = useState([])
 
   useEffect(() => {
     axios
       .get('http://restcountries.eu/rest/v2/all')
       .then(response => setCountries(response.data) )
   }, [])
+
+  useEffect(() => {
+    axios
+      .get('http://api.weatherstack.com/current')
+      .then(response => setWeather(response.data))
+  }, [])
+  
+  console.log('weather', weather)
+
   const handleFilter = (event) => setFilter(event.target.value)
 
-  const countriesToShow = countries.filter(countrie => countrie.name.toLowerCase().includes(filter.toLowerCase()))
+  const countriesToShow = countries.filter(country => country.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <>
@@ -78,7 +97,7 @@ const App = () => {
 	onChange={handleFilter}
       />
       <div>
-	<Countries countries={countriesToShow} />
+	<Countries countries={countriesToShow} setFilter={setFilter} />
       </div>
     </>
   )
